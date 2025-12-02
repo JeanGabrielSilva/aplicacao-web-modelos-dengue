959429cc6f6d4572ed91d42274999f2fe35e917e
JeanGabrielSilva <jeangabrielslima@gmail.com>
2025-12-01 22:42:55 -0300

    Removendo arquivo inuteis

--- backend/app.py
+++ backend/app.py
@@ -122,6 +122,4 @@ def predict(data: PredictRequest):
 # 🔹 Rodar local
 # ================================
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)
